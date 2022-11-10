28974393970b34210f97e3d36298d2e7bcb8e6cd
ShroomsAreBad <117897912+ShroomsAreBad@users.noreply.github.com>
2022-11-11 00:05:47 +0800

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,15 @@
+function Answer1(){
+	document.getElementById("answer1").innerHTML="The answer is 15";
+}
+
+function Answer2(){
+	document.getElementById("answer2").innerHTML="The answer is 5";
+}
+
+function Answer3(){
+	document.getElementById("answer3").innerHTML="The answer is 2";
+}
+
+function Answer4(){
+	document.getElementById("answer4").innerHTML="The answer is 50";
+}
